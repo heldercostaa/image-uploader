c782b98ad8f8e43fb2bbcad3608e285007941133
heldercostaa <heldercostaaa@gmail.com>
2026-04-06 01:49:53 -0300

    feat: customize files dropzone

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from 'react';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 const buttonVariants = tv({
-  base: 'text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none',
+  base: 'text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none cursor-pointer',
 
   variants: {
     size: {
