9aa98485cd519332a17c328e1f02934df69e68bb
heldercostaa <heldercostaaa@gmail.com>
2026-04-07 01:54:39 -0300

    feat: add expand, collapse and opacity animations with motion

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,5 +1,6 @@
+import { motion } from 'motion/react';
 import { useDropzone } from 'react-dropzone';
-import { CircularProgressBar } from './ui/circulas-progress-bar';
+import { CircularProgressBar } from './ui/circular-progress-bar';
 
 export function UploadWidgetDropzone() {
   const isThereAnyPendingUpload = true;
@@ -17,7 +18,12 @@ export function UploadWidgetDropzone() {
   });
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <motion.div
+      className="px-3 flex flex-col gap-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+    >
       <div
         data-active={isDragActive}
         className="cursor-pointer text-zinc-400 bg-black/20 pg-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400"
@@ -39,6 +45,6 @@ export function UploadWidgetDropzone() {
       </div>
 
       <span className="text-xxs text-zinc-400">Only PNG and JPG files are supported.</span>
-    </div>
+    </motion.div>
   );
 }
