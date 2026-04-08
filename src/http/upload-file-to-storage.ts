b3c3638993b0d0018b365f97fc69c55d9d83890c
heldercostaa <heldercostaaa@gmail.com>
2026-04-08 02:12:02 -0300

    feat: add upload status canceled and error; adjust buttons disabled statuses

--- src/http/upload-file-to-storage.ts
+++ src/http/upload-file-to-storage.ts
@@ -4,7 +4,14 @@ type UploadFileToStorageParams = {
   file: File;
 };
 
-export async function uploadFileToStorage({ file }: UploadFileToStorageParams) {
+type UploadFileToStorageOpts = {
+  signal?: AbortSignal;
+};
+
+export async function uploadFileToStorage(
+  { file }: UploadFileToStorageParams,
+  opts?: UploadFileToStorageOpts
+) {
   const data = new FormData();
 
   data.append('file', file);
@@ -13,6 +20,7 @@ export async function uploadFileToStorage({ file }: UploadFileToStorageParams) {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
+    signal: opts?.signal,
   });
 
   return { url: response.data.url };
