72382e5b9e430792dd7c6580ea66296c13be6ab5
heldercostaa <heldercostaaa@gmail.com>
2026-04-08 02:43:59 -0300

    feat: update global upload progress

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,18 +1,19 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { motion, useCycle } from 'motion/react';
+import { usePendingUploads } from '../store/uploads';
 import { UploadWidgetDropzone } from './upload-widget-dropzone';
 import { UploadWidgetHeader } from './upload-widget-header';
 import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button';
 import { UploadWidgetUploadList } from './upload-widget-upload-list';
 
 export function UploadWidget() {
-  const isThereAnyPendingUpload = true;
+  const { isThereAnyPendingUploads } = usePendingUploads();
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
   return (
     <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
       <motion.div
-        data-progress={isThereAnyPendingUpload}
+        data-progress={isThereAnyPendingUploads}
         className="bg-zinc-900 overflow-hidden rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
         animate={isWidgetOpen ? 'open' : 'closed'}
         variants={{
